934fce0a9863425069915e3a78991fb34b98ac54
Shleenkury <herooftime254@yahoo.com>
2025-07-02 15:04:13 -0500

    16x16 default grid and hover added with transition

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const Button = document.getElementById("amount");
+
+
 const Container = document.getElementById("container");
+
+
 
 const Square = document.querySelector(".square");
 Square.addEventListener("transitionend", () => {
@@ -7,9 +12,32 @@ Square.addEventListener("transitionend", () => {
 
 
 
-
-
 function addSquare() {
-    let square = document.createElement("div");
-    }
+    const newDiv = document.createElement("div")
+    newDiv.classList.add("square")
+    Container.appendChild(newDiv);
+    newDiv.addEventListener("transitionend", () => {
+    newDiv.style.opacity = 1;
+}, {once: true});
+}
+
+
+
+for(let i = 0; i < 256; i++) {
+    addSquare() 
+}
+
+
+Button.addEventListener("click", () => {
+    userInput = prompt("Enter amount:");
+    console.log(userInput);
+    for(i = 0; i < userInput; i++) {
+    addSquare() 
+}
+
+
+});
+
+
+
 
